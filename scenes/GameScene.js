0c4fd0236caf48f1b8af7badc1c97fb9b91caa93
Kinga L <memoey99@gmail.com>
2021-05-15 12:59:59 +0200

    dodanie tekstu z wynikiem i życiem, reset żyć i wyniku

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -8,13 +8,12 @@ export default class GameScene extends Phaser.Scene {
     this.enemies = [];
     this.gems;
     this.score = 0;
-    this.scoreText;
   }
 
   preload = () => {
     this.load.crossOrigin = "anonymous";
-    //this.load.image("background", "../assets/background3.png");
-    this.load.bitmapFont("pixelFont","assets/font.png","assets/font.xml");
+
+    this.load.bitmapFont("pixelFont", "assets/font.png", "assets/font.xml");
     this.load.image("platform", "https://labs.phaser.io/assets/sprites/block.png");
     this.load.image('gem', 'https://labs.phaser.io/assets/sprites/gem.png');
     this.load.spritesheet("enemy", "https://labs.phaser.io/assets/animations/nyan/cat.png", {
@@ -35,9 +34,6 @@ export default class GameScene extends Phaser.Scene {
   };
 
   create = () => {
-
-    this.scoreText = this.add.text(16,16,"SCORE:")
-
     var back = this.add.tileSprite(0, -40, WIDTH, HEIGHT, "background");
     back.setOrigin(0);
     back.setScrollFactor(0);
@@ -122,7 +118,7 @@ export default class GameScene extends Phaser.Scene {
     });
 
 
-    this.player.setCollideWorldBounds(true,true, false, true);
+    this.player.setCollideWorldBounds(true, true, false, true);
     this.player.body.onWorldBounds = true;
     this.player.setBounce(0.1);
     this.cameras.main.startFollow(this.player);
@@ -132,7 +128,7 @@ export default class GameScene extends Phaser.Scene {
     this.enemies = [];
     //Tworzenie przeciwników
     let enemy = this.physics.add.sprite(120, 500, "enemy");
-   
+
     enemy.immovable = true
     // enemy.hp = new HealthBar(this,enemy.x,enemy.y);
     enemy.body.gravity.y = 500;
@@ -145,19 +141,19 @@ export default class GameScene extends Phaser.Scene {
     this.enemies.push(enemy);
 
     this.gems = this.physics.add.group({
-      key:"gem",
-      repeat:100,
-      setXY:{x:100,y:200,stepX:150},
+      key: "gem",
+      repeat: 100,
+      setXY: { x: 100, y: 200, stepX: 150 },
     });
 
-    this.gems.children.iterate(function (child){
-      child.setBounceY(Phaser.Math.FloatBetween(0.1,0.3));
+    this.gems.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.3));
       child.setScale(0.3)
     })
 
     //Player colliders
     this.physics.add.collider(this.player, layer);
-    this.physics.add.overlap(this.player, enemy,this.dealDamage,null,this);
+    this.physics.add.overlap(this.player, enemy, this.dealDamage, null, this);
 
     //Enemy battle system
     this.input.keyboard.on("keydown-Z", () => {
@@ -171,12 +167,26 @@ export default class GameScene extends Phaser.Scene {
     });
 
     //Gems colliders
-    this.physics.add.collider(this.gems,layer);
-    this.physics.add.collider(this.gems,this.player,this.collectGem)
+    this.physics.add.collider(this.gems, layer);
+    this.physics.add.collider(this.gems, this.player, this.collectGem);
+
+    //Score text
+    var style = {
+      font: "pixelFont",
+      fill: "#000000",
+      align: "center"
+    }
+
+    this.scoreText = this.add.text(16, 16, "Score:", style);
+    this.livesText = this.add.text(16, 32, "Lives:", style);
+    this.updateText();
+    //tekst podążą za kamerą
+    this.scoreText.setScrollFactor(0);
+    this.livesText.setScrollFactor(0);
   };
 
   update = () => {
-  
+
     let cursors = this.cursors;
     let player = this.player;
     if (cursors.left.isDown) {
@@ -192,8 +202,8 @@ export default class GameScene extends Phaser.Scene {
     }
 
     if (this.enemies.length != 0)
-    this.enemies.forEach(o => o.anims.play("evil_walk", true));
-    
+      this.enemies.forEach(o => o.anims.play("evil_walk", true));
+
     //this.physics.moveToObject(this.enemies[0],player,200)
 
     if (
@@ -211,6 +221,8 @@ export default class GameScene extends Phaser.Scene {
   };
 
   start = () => {
+    this.lives = 5;
+    this.score = 0;
     this.scene.start("TitleCardScene");
   };
 
@@ -221,27 +233,35 @@ export default class GameScene extends Phaser.Scene {
       console.log(lives);
     }
     if (this.lives == 0) {
+      //reset lives and score
+      this.lives = 5;
+      this.score = 0;
       this.scene.start("GameOverScene")
-    }if(down && this.lives > 0){
+    } if (down && this.lives > 0) {
       let lives_left = this.lives
       this.registry.destroy()
       this.events.off()
       this.scene.restart()
       this.lives = lives_left
     }
+    this.updateText();
   };
 
-  dealDamage = () =>{
+  dealDamage = () => {
 
-  
+
 
   }
 
-  collectGem = (player,star) =>{
-    star.disableBody(true,true)
-    this.score += 10
-    this.scoreText.setText('Score: ' + this.score)
+  collectGem = (player, star) => {
+    star.disableBody(true, true);
+    this.score += 10;
+    this.updateText();
     console.log("SCORE: " + this.score, "scoreText: " + this.scoreText.getText)
   }
 
+  updateText = () => {
+    this.scoreText.setText('Score: ' + this.score);
+    this.livesText.setText('Lives: ' + this.lives);
+  }
 }
